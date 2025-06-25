785a05f6274a0ab22b09d930e7caccd2a1e6909c
gabriel nardelli <gv.nardelli@gmail.com>
2025-06-25 10:47:04 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 /// <reference types="@angular/localize" />
 
 import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
-bootstrapApplication(App, {
-  providers: [
-    { provide: LocationStrategy, useClass: HashLocationStrategy }
-  ]
-});
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
