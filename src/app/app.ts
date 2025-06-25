fa5d1da1b4f721dcaefc92a28f765dac60712a40
gabriel nardelli <gv.nardelli@gmail.com>
2025-06-25 10:43:10 -0300

    mudando para usar o hashstrategy

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,12 +3,10 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [ RouterOutlet, MatSidenavModule, MatListModule, MatIconModule, RouterLink],
-  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 /// <reference types="@angular/localize" />
 
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ]
+});
