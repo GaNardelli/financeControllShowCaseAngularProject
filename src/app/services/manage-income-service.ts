64cfdf9a4de382b5e29c4109e5d0c06af73ec000
Gabriel Nardelli <gv.nardelli@gmail.com>
2025-07-01 08:37:19 -0300

    criando branch de desenvolvimento

--- src/app/services/manage-income-service.ts
+++ src/app/services/manage-income-service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Income } from '../model/income.type';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,23 +9,24 @@ import { Income } from '../model/income.type';
 export class ManageIncomeService {  
   http = inject(HttpClient);
   constructor() { }
+  baseUrl = `${environment.apiUrl}`;
   getIncomesFromApi(income_id?: number) {
-    let url = 'https://sometesteapi.com/income_list';
+    let url = `${this.baseUrl}/income_list`;
     if (income_id && income_id > 0) {
       url += `?income_id=`+income_id;
     }
     return this.http.get<Array<Income>>(url);
   }
   deleteIncomeFromApi(income_id: number) {
-    let url = 'https://sometesteapi.com/income_ctr?func=deleletedIncome&income_id='+income_id;
+    let url = `${this.baseUrl}/deleletedIncome?income_id=`+income_id;
     return this.http.delete<Array<Income>>(url);
   }
   addNewIncome(new_income_object: Income) {
-    let url = `https://sometesteapi.com/income_ctr?func=addNewdIncome`;
+    let url = `${this.baseUrl}/addNewdIncome`;
     return this.http.post<Array<Income>>(url , new_income_object);
   }
   updateIncome(update_income: any) {
-    let url = `https://sometesteapi.com/income_ctr?func=updatedIncome`;
+    let url = `${this.baseUrl}/updatedIncome`;
     return this.http.post<Array<Income>>(url , update_income);
   }
 }
